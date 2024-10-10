9671f6a52746ccd06e087134efe00d8da58edefe
bijoor <ashutosh.bijoor@accionlabs.com>
2024-10-10 19:47:24 +0530

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -10,5 +10,5 @@ export default defineConfig(({ command, mode }) => ({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  base: mode === 'production' ? '/isometric-compiler-ui/' : '/',
+  base: '/isometric-compiler-ui/',
 }))
